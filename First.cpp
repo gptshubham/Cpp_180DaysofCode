978daa17d3a295a5672ee1d0f219da9f7ee6d9ac
ShubhamKrGupta <shubhamca76@gmail.com>
2023-09-21 12:54:16 +0530

    initial commit of First c++ program

---
+++ First.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// // variable declaration
+// int a;
+// a = 10; // variable defined
+// // int b = 11, c = 12, d = 13, e = 14;
+// // cout << a + b;
+// char c = 'a';
+// cout << c;
+// float f = 1.23;
+// cout << f;
+// double d = 3.121;
+// cout << d;
+//     bool b = 0, g = true;
+//     cout << b << endl;
+//     cout << g;
+// };
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "shubham";
+// };
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string name;
+//     name = "shubham";
+//     cout << name;
+// };

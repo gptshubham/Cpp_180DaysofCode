4bc1899ae1ae73be5b4dd5326c9b5882e2d75ee4
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-09-22 14:24:10 +0530

    including Else-if and examples

--- if-else.cpp
+++ if-else.cpp
@@ -4,13 +4,87 @@ int main()
 {
     // package example
     // int package;
+    // cout << "Enter Package : ";
+    // cin >> package;
+    // if (package > 10)
+    // {
+    //     cout << "Accepted!";
+    // }
+    // else
+    // {
+    //     cout << "Rejected!";
+    // }
 
     // marks example
-    int marks;
-    cout << "Enter Your Marks : ";
-    cin >> marks;
-    if (marks > 33)
-        cout << "Pass!"; // if there is only single line of code after if then no need of {}
-    else
-        cout << "Fail!"; // if there is only single line of code after else then no need of {}
+    // int marks;
+    // cout << "Enter Your Marks : ";
+    // cin >> marks;
+    // if (marks > 33)
+    // cout << "Pass!"; // if there is only single line of code after if then no need of {}
+    // else
+    // cout << "Fail!"; // if there is only single line of code after else then no need of {}
+
+    // a>b example
+    // int a, b;
+    // cout << "Enter the value of a and b: ";
+    // cin >> a >> b;
+    // if (a > b)
+    // cout << "yes!";
+    // else
+    // cout << "No!";
+
+    // Even or Odd Example
+    // int number;
+    // cout << "Enter a Number: ";
+    // cin >> number;
+    // if (number % 2 == 0)
+    //     cout << "Even Number";
+    // else
+    //     cout << "Odd Number";
+
+    // nested if-else Example with Alternative Else If
+    // int number;
+    // cout << "Enter a Number: ";
+    // cin >> number;
+    // if (number > 0)
+    // {
+    //     cout << "Positive Number";
+    // }
+    // else
+    // {
+    //     if (number == 0)
+    //     {
+    //         cout << 0;
+    //     }
+    //     else
+    //     {
+    //         cout << "Negative Number";
+    //     }
+    // }
+    // Alternatively - using Else If
+    // if (number > 0)
+    //     cout << "Positive Number";
+    // else if (number == 0)
+    //     cout << 0;
+    // else
+    //     cout << "Negative Number";
+
+    // Vowel Consonant Example
+    // char c;
+    // cout << "Enter a Character: ";
+    // cin >> c;
+    // if (c == 'a')
+    //     cout << "Vowel";
+    // else if (c == 'e')
+    //     cout << "Vowel";
+    // else if (c == 'i')
+    //     cout << "Vowel";
+    // else if (c == 'o')
+    //     cout << "Vowel";
+    // else if (c == 'u')
+    //     cout << "Vowel";
+    // else
+    //     cout << "Consonant";
+
+    // coder army printing 5 times on the screen
 }

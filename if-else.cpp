99dd84452d5cecbff391d4c6caa351e5b61145df
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-09-23 06:01:56 +0530

    complete lec4 if-else and loop

--- if-else.cpp
+++ if-else.cpp
@@ -86,5 +86,56 @@ int main()
     // else
     //     cout << "Consonant";
 
+    /*
     // coder army printing 5 times on the screen
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Coder Army\n";
+    }*/
+
+    /*
+    // printing first n natural numbers
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }*/
+
+    /*
+    // printing squares of first n natural numbers
+    int n;
+    cout << "Enter The Number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " Square is: " << i * i << endl;
+    }*/
+
+    /*
+    // printing all Even Numbers upto n
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    // for (int i = 2; i <= n; i = i + 2)
+    // {
+    //     cout << i << endl;
+    // }
+    // Alternatively
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            cout << i << " ";
+    }*/
+
+    /*
+    // Print all numbers from 1 to n, which is divisible by 4. Take n as an input from the user.
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 4 == 0)
+            cout << i << ",";
+    }*/
 }

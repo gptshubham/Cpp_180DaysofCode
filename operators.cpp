1d677a5e18fa5b6c06c8988ccc171243ad413b5e
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-09-29 09:20:37 +0530

    complete operator session

---
+++ operators.cpp
@@ -0,0 +1,77 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << 13 / 2 << endl;
+    // cout << 13.2 / 2 << endl;
+    // cout << 13 * 4 << endl;
+    // cout << 4 * 13.2;
+    // incrementation,deincrementation
+    int a = 10;
+    // int b = a++;
+    int b = ++a;
+    // cout << a++ << endl;
+    // cout << a;
+    cout << b << endl;
+    cout << a;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // comparison operator : same as python , just that the output is 0 or 1 and remaining operations are executed accordingly.
+    //Output : Boolean value
+    //Operator Precedence and Associativity should be kept in mind.;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Logical Operators - same as JavaScript;
+    // Output : Boolean Values;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Bitwise Operator
+
+    // & Operator
+    // int ans = 2 & 3;
+    // cout << ans;
+
+    // | Operator
+    // int ans = 2 | 3;
+    // cout << ans;
+
+    // ^ Operator
+    // int ans = 2 ^ 3;
+    // cout << ans;
+
+    // << Left Shift
+    // int ans = 2 << 3;
+    // cout << ans;
+
+    // >> Right Shift
+    // int ans = 20 >> 3;
+    // cout << ans;
+
+    // ~ Complement
+    int ans = ~5;
+    cout << ans;
+
+    // = Assignment Operator
+    // same as python : =,+=,-=,*=,/=,%=;
+}
+*/
